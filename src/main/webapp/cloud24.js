fbb9817e4143a7ebdd5e66e5aec71bf7a1bbef8b
Manuel Clavel <manuelgclavel@gmail.com>
2024-10-23 14:39:43 +0700

    working on it

--- src/main/webapp/cloud24.js
+++ src/main/webapp/cloud24.js
@@ -8,4 +8,7 @@ click_button.addEventListener("click", function(){
     alert("Hi!");
     //event.target.style.background = "red";
     click_button.style.background = "red";
+    
+    
+    Date.parse("2024-10-23T07:37:10.013394Z");
 })
