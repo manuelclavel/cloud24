255b0a7459025ab83e765ca901b05a49a843b50f
Manuel Clavel <manuelgclavel@gmail.com>
2024-10-23 13:30:24 +0700

    working on it

---
+++ src/main/java/ServerTime.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.time.LocalDate;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author clavel
+ */
+@WebServlet(urlPatterns = {"/server_time"})
+public class ServerTime extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(resp.getOutputStream(), "UTF-8"));
+
+        resp.setContentType("text/plain");
+        resp.setStatus(200);
+        LocalDate localDate = LocalDate.now();
+        writer.write(localDate.toString());
+        writer.flush();
+        writer.close();
+    }
+}
---
+++ src/main/webapp/cloud24.js
@@ -0,0 +1,11 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+let click_button = document.getElementById("click_button");
+click_button.addEventListener("click", function(){
+    alert("Hi!");
+    //event.target.style.background = "red";
+    click_button.style.background = "red";
+})
